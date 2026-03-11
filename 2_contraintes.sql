5d1ecfc933e2ba0bea6b3c4342fcbb8884cb04c9
justachillguy <lucas.singhavara@efrei.net>
2026-03-11 11:46:42 +0100

    Update constraints for ARTIST and TRACK tables
    Reorganize constraints for TRACK and remove unnecessary checks.

--- 2_contraintes.sql
+++ 2_contraintes.sql
@@ -1,22 +1,17 @@
 ALTER TABLE ARTIST 
 ADD CONSTRAINT chk_artist_age CHECK (Age >= 16) 
 
-ALTER TABLE TRACK
-ADD CONSTRAINT chk_track_duration CHECK (Duration_Sec >= 90);
-
 ALTER TABLE ARTIST
 ADD CONSTRAINT chk_artist_position 
 CHECK (Position_ IN ('dancer', 'singer', 'rapper', 'visual'));
+  
+ALTER TABLE TRACK
+ADD CONSTRAINT chk_track_duration CHECK (Duration_Sec >= 90);
 
+ALTER TABLE TRACK 
+ADD CONSTRAINT chk_track_bpm_range 
+CHECK (BPM BETWEEN 40 AND 300);
 
 ALTER TABLE FAN 
 ADD CONSTRAINT chk_fan_email 
 CHECK (Fan_Email LIKE '%_@__%.__%');
-
-ALTER TABLE TRACK 
-ADD CONSTRAINT chk_track_bpm 
-CHECK (BPM > 0);
-
-ALTER TABLE FANCLUB 
-ADD CONSTRAINT chk_fanclub_creation 
-CHECK (Creation_Date <= CURRENT_DATE);
